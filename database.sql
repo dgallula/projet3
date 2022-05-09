02c664b09a59fe84f241f2537f393d16625039f5
Your Name <you@example.com>
2022-05-09 08:24:20 +0300

    ok

--- database.sql
+++ database.sql
@@ -1,29 +1,25 @@
 create database vacations;
 
-
 use vacations;
 
 
 CREATE TABLE users (
 id   INT NOT NULL AUTO_INCREMENT ,
-name VARCHAR(255) not null,
+firstname VARCHAR(255) not null,
 familyName varchar(255) not null,
-username varchar(255) not null,
-password int not null,
+email varchar(255)unique not null,
+username varchar(255)  not null,
+password int unique not null ,
 role varchar(255) not null,
 PRIMARY KEY (id)
 );
 
 
-INSERT INTO users (name, familyName, username, password, role)
+INSERT INTO users (firstName, familyName, email, username, password, role)
 VALUES
-('David', 'Gallula', 'dgallula@gmail.com', 1234, 'admin' ),
-('Elie', 'Zenou', 'Elie@gmail.com', 1234, 'user' ),
-('Elie', 'Sellem', 'Elisellem@gmail.com', 1234, 'user' ),
-('Helene', 'Zenou', 'Helene@gmail.com', 1234,'user' ),
-('Levana', 'Amoyalle', 'Levana@gmail.com', 1234,'user' ),
-('Levy', 'Cohen', 'levy@gmail.com', 1234, 'user' ),
-('Perez', 'Avraham', 'Avi@gmail.com', 1234,'user' );
+('David', 'Gallula', 'dgallula@gmail.com','dgallula', 1234, 'admin' ),
+('Elie', 'Zenou', 'elie@gmail.com','elie', 1235, 'user' );
+
 
 
 
@@ -36,22 +32,18 @@ img VARCHAR(255) not null,
 price int not null,
 DateFrom datetime not null,
 DateUntil datetime not null,
+followers int,
 userId int not null,
 PRIMARY KEY (id),
 FOREIGN KEY(userId) REFERENCES users(id)
 );
 
-INSERT INTO vacations (cityName,country, descriptions, img, price, dateFrom, dateUntil, userId)
+INSERT INTO vacations ( descriptions, cityName, country, img, price, dateFrom, dateUntil,followers, userId)
 VALUES
 
-('Paris', 'France','Paris la ville des amoureux','Paris.jpej', 499, '2022-06-23', '2022-06-23',  1),
-('Madrid' ,'Espagne','Viva espagna','Madrid.jpej', 399, '2020-07-23', '2022-07-23', 2),
-('Courchevelle','France','La montagne , Ca vous gagne','Courchevelle.jpej', 699, '2022-08-23', '2022-08-23', 2),
-('Miami', 'USA','une destination tres prisee ','Miami.webp', 899, '2022-08-23', '2022-08-23', 1),
-('New york', 'USA','New York new york  ','NY.jpej', 899, '2022-09-23', '2022-09-23', 3),
-('Jerusalem', 'Israel', 'la ville des trois religions ','Jerusalem.jpej', 999, '2022-10-23', '2022-10-23',4),
-('Bruxelle', 'Belgium','une ville tres agrable ','Belgium.jpej', 399, '2022-10-23', '2022-10-23', 4);
-
+('Paris', 'France','Paris la ville des amoureux','Paris.jpej', 499, '2022-06-23', '2022-06-23',5000,  1),
+('Madrid' ,'Espagne','Viva espagna','Madrid.jpej', 399, '2020-07-23', '2022-07-23',4000, 2),
+('Courchevelle','France','La montagne , ça vous gagne','Courchevelle.jpej', 699, '2022-08-23', '2022-08-23',6000, 2);
 
 
 CREATE TABLE follow (
@@ -67,12 +59,35 @@ FOREIGN KEY(vacationId) REFERENCES vacations(id)
 INSERT INTO follow ( userId, vacationId,followers)
 VALUES
 (1,1,5000),
-(1,2,4000),
-(1,3,6000),
-(1,4,10000),
-(1,5,8000),
-(1,6,20000),
-(1,7,40000);
+(2,2,4000),
+(1,3,6000);
+
+CREATE TABLE followedVacations (
+    id INT AUTO_INCREMENT NOT NULL,
+    userID INT NOT NULL,
+    vacationID INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(userID) REFERENCES users(id),
+    FOREIGN KEY(vacationID) REFERENCES vacations(id)
+);
+
+
+INSERT INTO followedVacations ( userId, vacationId)
+VALUES
+(1,1),
+(2,2),
+(1,3);
+
+
+
+select* from vacations;
+
+select *from users;
+
+select * from followVacations;
+
+select * from follow;
+
 
 
 
--- server/business-logic/users-bl.js
+++ server/business-logic/users-bl.js
@@ -1,37 +1,72 @@
 import usersDal from "../data-access-layer/users-dal.js";
 
+
 const getAll = () => {
   return usersDal.getAll();
 };
 
-const getUserBy = (email) => {
-  return usersDal.getUserByEmail(email);
-};
+const getUser = async (userName, password) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
-const getAdmin = () => {
-  return usersDal.getAdmin();
-};
+  let result = await usersDal.getUser(userName);
+
+  if (result.data.length > 0) {
+      let check = bcrypt.compareSync(password, result.data[0].password);
+      if (check) {
+          return result
+      }
+      else {
+          result.success = false;
+          result.data = "Wrong password";
+          return result
+      }
+  }
+  else {
+      result.success = false;
+      result.data = "User not found";
+  }
+  return result
+}
 
 
-const getByUsers = () => {
-  return usersDal.getByUsers();
-};
+const addUser = async (body) => {
+  let password = body.password;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  return await usersDal.addUser(  body.firstName, body.familyName, body.email, body.userName, hash)
+}
+
+
+// const getUserBy = (email) => {
+//   return usersDal.getUserByEmail(email);
+// };
+
+// const getAdmin = () => {
+//   return usersDal.getAdmin();
+// };
+
+
+// const getByUsers = () => {
+//   return usersDal.getByUsers();
+// };
 
 
 
 
-const addUser = (newUser) => {
-  return usersDal.addNewUser(newUser);
-};
+// const addUser = (newUser) => {
+//   return usersDal.addNewUser(newUser);
+// };
 
-const updateUser = (id, user) => {
-  return usersDal.update(id, user);
-};
-export default {
-  getAdmin,
-  getByUsers,
+// const updateUser = (id, user) => {
+//   return usersDal.update(id, user);
+// };
+
+export {
   getAll,
+  getUser,
   addUser,
-  getUserBy,
-  updateUser,
-};
+  // deleteUser,
+  // updateUser
+}
